56284ec52094f2b8c04207cac853713cffd8b480
Ddong-Min <110894447+Ddong-Min@users.noreply.github.com>
2023-04-06 02:17:46 +0900

    Add files via upload

--- 사칙연산 유효성 검사.cpp
+++ 사칙연산 유효성 검사.cpp
@@ -6,18 +6,17 @@ using namespace std;
 
 struct Node {
 	int key;
-	int data;
+	char data;
 	Node* left, * right;
 	int left_x, right_x;
 };
 
-constexpr size_t MAX_NODE = 10000;
+constexpr size_t MAX_NODE = 100;
 
 int node_count = 0;
 Node node_pool[MAX_NODE];
-int calcul;
 
-Node* new_node(int x, int data, int left, int right) {
+Node* new_node(int x, char data, int left, int right) {
 	node_pool[node_count].key = x;
 	node_pool[node_count].data = data;
 	node_pool[node_count].left = nullptr;
@@ -39,17 +38,17 @@ public:
 		node_count = 0;
 	}
 
-	void insert(int x, int data, int left, int right) {
+	void insert(int x, char data, int left, int right) {
 		root = insert_rec(root, x, data, left, right, 1);
 	}
 
 	void traversal() const {
-		calcul = traversal_rec(root);
+		traversal_rec(root);
 	}
 
 
 public:
-	Node* insert_rec(Node* node, int x, int data, int left, int right, int T) {
+	Node* insert_rec(Node* node, int x, char data, int left, int right, int T) {
 		if (node == nullptr && T == 1) return new_node(x, data, left, right);
 		else if (node == nullptr && T == 0) return nullptr;
 
@@ -66,26 +65,14 @@ public:
 		return node;
 	}
 
-	int traversal_rec(Node* node) const {
-		if (node == nullptr) return 0;
-		int left_data, right_data;
+	void traversal_rec(Node* node) const {
+		if (node == nullptr) return;
 
-		left_data = traversal_rec(node->left);
-		
-		right_data = traversal_rec(node->right);
+		traversal_rec(node->left);
 
-		if (node->data == -1 || node->data == -2 || node->data == -3 || node->data == -4) {
+		std::cout << node->data;
 
-			int result = node->data;
-			if (node->data == -1) node->data = left_data + right_data;
-			if (node->data == -2) node->data = left_data - right_data;
-			if (node->data == -3) node->data = left_data * right_data;
-			if (node->data == -4) node->data = left_data / right_data;
-		}
-		return node->data;
-
-
-
+		traversal_rec(node->right);
 	}
 };
 
@@ -98,31 +85,30 @@ int main(int argc, char** argv)
 	{
 		tree.init();
 		int x, left, right, size;
-		int i, data2;
-		string data;
+		int i;
+		char data;
 		cin >> size;
 
-		for (i = 1; i <= size; i++) {
-			cin >> x >> data;
-			if (data == "+" || data == "-" || data == "*" || data == "/") {
-				cin >> left >> right;
-				if (data == "+") data2 = -1;
-				if (data == "-") data2 = -2;
-				if (data == "*") data2 = -3;
-				if (data == "/") data2 = -4;
-				tree.insert(x, data2, left, right);
-			}
-			else {
-				data2 = stoi(data);
-				tree.insert(x, data2, 0, 0);
-			}
-
-
+		for (i = 1; i <= (size - 1) / 2; i++) {
+			cin >> x >> data >> left >> right;
+			tree.insert(x, data, left, right);
 		}
 
+		if (size % 2 == 0) {
+			cin >> x >> data >> left;
+			tree.insert(x, data, left, 0);
+		}
+		else {
+			cin >> x >> data;
+			tree.insert(x, data, 0, 0);
+		}
+		for (i;i < size;i++) {
+			cin >> x >> data;
+			tree.insert(x, data, 0, 0);
+		}
 		cout << '#' << test_case << ' ';
 		tree.traversal();
-		cout << calcul << endl;
+		cout << endl;
 
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
